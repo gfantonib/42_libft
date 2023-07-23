4abf89f4f713d7a70625a662bc7b8c91f64f7ecc
gfantoni <guilhermefantoniborges@gmail.com>
2023-07-22 21:23:54 -0300

    commit 4

--- part1_libc_functions/testing_now/libft.h
+++ part1_libc_functions/testing_now/libft.h
@@ -6,11 +6,11 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 13:02:21 by gfantoni          #+#    #+#             */
-/*   Updated: 2023/07/22 21:14:10 by gfantoni         ###   ########.fr       */
+/*   Updated: 2023/07/22 21:21:55 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <stdlib.h>
+#include <stdlib.h>
 
 size_t	ft_strlen(const char *str);
 int	ft_isalpha(int c);
@@ -18,3 +18,4 @@ int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
+void    *ft_memset(void *b, int c, size_t len);
