43bf7f4c09a20a93d9fdfbc434f7e72115a006f2
Guilherme Fantoni Borges <gfantoni@c1r2p1.42sp.org.br>
2023-08-05 16:37:02 -0300

    final commit?

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: gfantoni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 09:30:27 by gfantoni          #+#    #+#             */
-/*   Updated: 2023/08/05 12:19:28 by gfantoni         ###   ########.fr       */
+/*   Updated: 2023/08/05 16:35:55 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,3 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 	ft_strlcpy(ret, s + start, len + 1);
 	return (ret);
 }
-
-//char	*ft_substr(char const *s, unsigned int start, size_t len)
-//{
-//	char	*new_str;
-//	size_t	i;
-//	size_t	j;
-//
-//	if (!s || !(new_str = (char *)malloc(len + 1)))
-//		return (0);
-//	i = start;
-//	j = 0;
-//	while (i < ft_strlen(s) && j < len)
-//		new_str[j++] = s[i++];
-//	new_str[j] = '\0';
-//	return (new_str);
-//}
