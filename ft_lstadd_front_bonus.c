c03776b462d4fd4ea89b37f54e052979816ee774
gfantoni <guilhermefantoniborges@gmail.com>
2023-08-14 12:09:29 -0300

    marvin

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -21,8 +21,3 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 		*lst = new;
 	}
 }
-//lst: The address of a pointer to the first link of
-//a list.
-//new: The address of a pointer to the node to be
-//added to the list.
-//Adds the node ’new’ at the beginning of the list
