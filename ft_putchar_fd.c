8fb5ce7e9edf48324fe9fb2d40b4168325c728a9
gfantoni <guilhermefantoniborges@gmail.com>
2024-06-25 21:43:30 -0300

    ft_putstr_fd.c and ft_putchar_fd.c modfyied to return a int in order to fit the ft_printf usage.

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,13 +6,14 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 09:36:07 by gfantoni          #+#    #+#             */
-/*   Updated: 2023/08/12 08:14:29 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/06/25 21:39:20 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putchar_fd(char c, int fd)
+int	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
+	return (1);
 }
